f75be6d82a878ef6c815aaf593e65d016f988702
Geoff Cranmer <geoff.cranmer@gmail.com>
2012-12-18 20:13:46 -0500

    Added test and fixed a number of errors

--- c++/Graph/graph.h
+++ c++/Graph/graph.h
@@ -1,10 +1,15 @@
 #include <iterator>
 #include <list>
+#include <map>
 #include <vector>
 
 // Fundamental question:
 // Should graphs be manipulated based on pointers or based on arbitrary indices?
 // See: Graph::Vertex::edges, Graph::Vertex::addEdge, and Graph::addEdge
+// Graphs should be manipulated based on pointers.
+// The vertex class will need to either be made public or mode out of the Graph
+// class altogether.
+// Create a constructor that takes a vector of vertices and a map of the edges.
 
 namespace Graph {
 
@@ -14,19 +19,22 @@ template <typename value_type> class Graph {
     class Vertex {
       private:
         value_type value;
-        std::list<int> edges;
+        std::list<Vertex*> edges;
 
       public:
         Vertex(value_type);
-        void addEdge(int);
+        void addEdge(Vertex*);
     };
 
     std::vector<Vertex> adjacency_list;
 
   public:
 
+    Graph(std::vector<value_type>, std::multimap<int, int>);
+    Graph();
+  
     void addVertex(value_type);
-    void addEdge(int, int);
+    void addEdge(Vertex*, Vertex*);
 
     class iterator : public std::iterator<std::forward_iterator_tag, Vertex> {
       private:
